0dd41d6a66009d7316831021599ca7a39d382ffa
karpovska <oksanachv@gmail.com>
2024-06-18 15:12:01 +0200

    Added option for reverting the pyramid

--- javascript algorithms and data structures/script.js
+++ javascript algorithms and data structures/script.js
@@ -1,13 +1,18 @@
-const character = "#";
-const count = 8;
+const character = "!";
+const count = 10;
 const rows = [];
+let inverted = false;
 
 function padRow(rowNumber, rowCount) {
 	return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i = i + 1) {
-	rows.push(padRow(i + 1, count));
+for (let i = 1; i <= count; i++) {
+	if (inverted) {
+		rows.unshift(padRow(i, count));
+	} else {
+		rows.push(padRow(i, count));
+	}
 }
 
 let result = "";
